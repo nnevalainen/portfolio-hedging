bce8eb7629ba459e0623d1b0b73c371ccba6b12c
Oskar Niemenoja <osku@scotch.local>
2018-12-17 15:01:57 +0200

    Added vega hedging

--- code/hedge_functions.py
+++ code/hedge_functions.py
@@ -1,28 +1,66 @@
-def delta_hedge(maturity, strike, price, q, sigma):
+import numpy as np
+import math
+import scipy.stats
 
-	# d1 from the Black-Scholes equation
-	d1 = (np.log(price/strike) + (q**2 + sigma**2/2) * maturity)/(sigma*math.sqrt(maturity))
+def calculate_implied_volatility_bs(maturity, strike, spot, q, Cobs, threshold = 0.01, initial = 0.5):
 
-	return scipy.stats.norm.cdf(d1)
-
-def calculate_implied_volatility_bs(maturity, strike, price, q, Cobs, threshold = 0.01, initial = 0.5):
-
-	sigmas = initial
+	# Initialize
+	sigma = initial
 	diff = 1
 
 	while diff > threshold:
-		# Calculate Black-Scholes price
-		d1 = (np.log(price/strike) + (q**2 + sigma**2/2) * maturity)/(sigma*math.sqrt(maturity))
-		d2 = d1 - sigma*math.sqrt(maturity)
-		bs = price * scipy.stats.norm.cdf(d1) - strike*math.exp(-q*maturity)*scipy.stats.norm.cdf(d2)
+		# Calculate Black-Scholes spot
+		d1 = (np.log(spot/strike) + (q**2 + sigma**2/2) * maturity)/(sigma*math.sqrt(maturity))
+		d2 = d1 - sigma * math.sqrt(maturity)
+		bs = spot * scipy.stats.norm.cdf(d1) - strike * math.exp(-q * maturity) * scipy.stats.norm.cdf(d2)
 
 		# First derivaive of BS w.r.t volatility
-		bs_der = price*math.sqrt(maturity)*math.exp(-d1**2/2)/(math.sqrt(2*math.pi))
+		bs_der = spot * math.sqrt(maturity) * math.exp(-d1**2 / 2) / (math.sqrt(2 * math.pi))
 
 		# Calculate the volatility difference
-		diff = (Cobs-bs)/bs_der
+		diff = (Cobs - bs) / bs_der
 
 		# updaede
 		sigma += diff
 
 	return sigma
+
+def delta(maturity, strike, spot, q, sigma):
+
+	# d1 from the Black-Scholes equation
+	d1 = (np.log(spot/strike) + (q**2 + sigma**2 / 2) * maturity)/(sigma*math.sqrt(maturity))
+
+	return scipy.stats.norm.cdf(d1)
+
+def vega(maturity, strike, spot, q, sigma):
+
+	# d1 from the Black-Scholes equation
+	d1 = (np.log(spot/strike) + (q**2 + sigma**2/2) * maturity) / (sigma * math.sqrt(maturity))
+
+	# Calculate vega value
+	vega = spot * math.exp(-d1**2 / 2)*math.sqrt(maturity) / (math.sqrt(2 * math.pi))
+
+	return vega
+
+def delta_hedge(maturity, strike, spot, q, sigma):
+
+	# Calculate delta. This is the number to short the underlying
+	delta_bs = delta(maturity, strike, spot, q, sigma)
+
+	return -delta_bs
+
+def vega_hedge(maturity_1, maturity_2, strike, spot, q, sigma):
+	
+	# Calculate hedged option and replication option deltas
+	delta_bs = delta(maturity_1, strike, spot, q, sigma)
+	delta_rep = delta(maturity_2, strike, spot, q, sigma)
+
+	# Calculate hedged option and replication option vegas
+	vega_bs = vega(maturity_1, strike, spot, q, sigma)
+	vega_rep = vega(maturity_2, strike, spot, q, sigma)
+
+	# Alpha is the amount of stock to hold - eta is the amount of replication stock to hold
+	alpha = -delta_bs + vega_bs / vega_rep * delta_rep
+	eta = - vega_bs / vega_rep
+
+	return alpha, eta
